820ac8aaefb648e260bc685652edfb0aefbe9b26
Oleg Usachev <usaechv97@mail.ru>
2023-08-20 19:38:24 +0500

    Реализовал класс в Item в `src/item.py`. Реализовал методы в соответствии с заданием. Сделал атрибут `name` сделать приватным. Добавил геттер и сеттер для `name`. Добавил в Item методы в соответствии с заданием 2. Написал тесты с процентом покрытия 82%

--- src/item.py
+++ src/item.py
@@ -1,7 +1,11 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
+
     pay_rate = 1.0
     all = []
 
@@ -13,7 +17,52 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.__name = name
+        self.price = price
+        self.quantity = quantity
+        # Возвращает сущности объектов класса в виде списка
+        self.all = Item.all.append(self)
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """
+        Функция распаковывает csv файл в виде словаря.
+        Создает экземпляры класса.
+        """
+        with open("C:/Users/irpro/PycharmProjects/electronics-shop-project/src/items.csv", newline="", encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for item in reader:
+                name, price, quantity = item['name'], item['price'], item['quantity']
+                newItem = cls(name, price, quantity)
+            return newItem
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """
+        Проверяет,  что длина наименования товара не больше
+        10 симвовов. В противном случае,
+        обрезаеть строку (оставляет первые 10 символов).
+        """
+        if len(name) > 10:
+            self.__name = name[:10]
+            print(f'{self.__name} -> длина наименования товара больше 10 символов')
+        else:
+            self.__name = name
+            print(f'{self.__name } -> длина наименования товара меньше 10 символов')
+
+    @staticmethod
+    def string_to_number(number):
+        """
+        Возвращает строку в формате int.
+        """
+        try:
+            return int(number)
+        except ValueError:
+            return int(float(number))
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +70,11 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
-    def apply_discount(self) -> None:
+    def apply_discount(self) -> float:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price * self.pay_rate
+        return self.price
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,56 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+""" 
+Импорт класса
+"""
+from src.item import Item
+import csv
+
+
+def test_count_object():
+    """
+    Тест счетчика количества объектов экземпляров класса
+     Item и корректность хранения объектов
+    """
+    Item("Телевизор", 12500, 220)
+    Item("Ноутбук", 70000, 15)
+    assert len(Item.all) == 2
+
+
+def test_calculate_total_price():
+    """
+    Тест метода класса на правильный
+    расчет общей стоимости конкретного товара в магазине.
+    """
+    item = Item("Смартфон", 100.0, 5)
+    assert item.price * item.quantity == 500.0
+
+
+def test_apply_discount():
+    """
+    Тест метода класса на корректное приминение
+    скидки.
+    """
+    item = Item("Клавиатура", 100.0, 5)
+    Item.pay_rate = 0.8
+    assert item.price * item.pay_rate == 80.0
+
+
+def test_name():
+    """
+    Тест метода класса на проверку
+    длинны товара
+    """
+    item = Item("Клавиатура", 100.0, 5)
+    item.name = 'СуперСмартфон'
+    assert len(item.name) == 10
+
+
+def test_string_to_number():
+    """
+    Тест статического метода,
+    возвращающий число из числа-строки.
+    """
+    assert Item.string_to_number('5.5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5') == 5
